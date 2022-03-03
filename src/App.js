3d09e838b4465768bdb2ff3d1bea0786a08900e0
mai <maifouad136@gmail.com>
2022-03-03 18:04:06 +0200

    fix navbar view

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import './App.css';
-import Navbar from './Components/Navbar/Navbar';
+import Home from './Pages/Home/Home';
 
 
 function App() {
   return (
     <div className="App">
-        
-        <Navbar/>
-        <h1>Hello World!</h1>
-        
+        <Home/>
     </div>
   );
 }
